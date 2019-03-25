d6efdff560843db09b498020b68c4f238359429d
michael butts <mgbutts@gmail.com>
2019-03-24 23:38:01 -0400

    Updates

---
+++ precache-manifest.cc0d61e9cb0f51da1c44ce2d14f0a086.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3ffd3696fea8999087bbdcf97d69d2",
+    "url": "/thevenue/static/media/slide_two.3a3ffd36.jpg"
+  },
+  {
+    "revision": "9aa09a50b7eb84e96d522c10dfaa78e5",
+    "url": "/thevenue/static/media/slide_three.9aa09a50.jpg"
+  },
+  {
+    "revision": "84753208e3ace6c36a3a29c731983a69",
+    "url": "/thevenue/static/media/slide_one.84753208.jpg"
+  },
+  {
+    "revision": "2dfd02fe05d7b2c52a81",
+    "url": "/thevenue/static/js/runtime~main.1a802491.js"
+  },
+  {
+    "revision": "2d187b143cb11a509cdb",
+    "url": "/thevenue/static/js/main.acf9d161.chunk.js"
+  },
+  {
+    "revision": "3e76410a0da7584d9443",
+    "url": "/thevenue/static/js/2.2218ae77.chunk.js"
+  },
+  {
+    "revision": "2d187b143cb11a509cdb",
+    "url": "/thevenue/static/css/main.a41f3302.chunk.css"
+  },
+  {
+    "revision": "f93ae8de7f4b698d3bd168fb3b5c2590",
+    "url": "/thevenue/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/thevenue/precache-manifest.cc0d61e9cb0f51da1c44ce2d14f0a086.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/thevenue/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
